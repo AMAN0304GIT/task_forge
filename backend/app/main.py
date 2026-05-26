a18bf56a220188e7815c1936a0c035cebc348c76
Aman Garg <amangarg@Amans-MacBook-Air.local>
2026-05-27 00:48:10 +0530

    Initial setup for Angular, FastAPI, PostgreSQL, and Docker

---
+++ backend/app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from app.core.database import Base, engine
+from app.models.user import User
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(
+    title="TaskForge API",
+    version="1.0.0"
+)
+
+@app.get("/")
+def root():
+    return {"message": "TaskForge API Running"}
